cfebbf536be8a9686dabfc2ab53458e58d0618fa
Kennyhip <nikitasobl1@gmail.com>
2023-05-24 16:38:48 +0600

    Hw 2

---
+++ HW 2.py
@@ -0,0 +1,17 @@
+# Задача 12: Петя и Катя – брат и сестра. Петя – студент, а Катя – школьница. Петя помогает Кате по математике.
+# Он задумывает два натуральных числа X и Y (X,Y≤1000), а Катя должна их отгадать. Для этого Петя делает две подсказки.
+# Он называет сумму этих чисел S и их произведение P. Помогите Кате отгадать задуманные Петей числа.
+import math
+
+def findNumbers(sumOfNumbers, prodOfNumbers):
+    discriminant = sumOfNumbers**2 - 4*prodOfNumbers
+    if discriminant < 0:
+        print(f'нет решения')
+    else:
+        x = (sumOfNumbers + math.sqrt(discriminant)) / 2
+        y = (sumOfNumbers - math.sqrt(discriminant)) / 2
+        print(f'Вы загадали числа {round(x)} и {round(y)}')
+
+sumOfNumbers = int(input("Введите сумму двух загаданных чисел: "))
+prodOfNumbers = int(input("Введите произведение этих же чисел: "))
+findNumbers(sumOfNumbers, prodOfNumbers)
